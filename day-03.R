6bb82254d755438b38e38126067fcf5c1f541bd1
Jindra Lacko <jindra@jla-data.net>
2024-12-03 08:44:23 +0100

    just in time for daily commute

---
+++ day-03.R
@@ -0,0 +1,45 @@
+input <- readLines("./input.txt")
+
+# part one
+
+mul_strings <- unlist(stringr::str_extract_all(input, "mul\\(\\d+,\\d+\\)"))
+
+ex_comma_strings <- stringr::str_replace_all(mul_strings, ",", "*")
+
+formula_strings <- stringr::str_remove_all(ex_comma_strings, "[mul\\(, \\)]")
+
+numbers <- sapply(formula_strings, \(x) eval(parse(text=x))) 
+
+print(paste("result of corrupted multiplication", sum(numbers)))
+
+# part two
+
+mul_strings <- unlist(stringr::str_extract_all(input, "mul\\(\\d+,\\d+\\)|do\\(\\)|don't\\(\\)"))
+
+active <- TRUE
+
+# iterate over mul_strings, removing 1) not active multiplication and 2) do / don't commands
+for (i in seq_along(mul_strings)) {
+   
+   if (mul_strings[i] == "don't()") {
+      active <- FALSE
+   }
+   
+   if (mul_strings[i] == "do()") {
+      active <- TRUE
+   }
+   
+   if(!active | stringr::str_starts(mul_strings[i], "do")) {
+      mul_strings[i] <- "0"
+   } 
+      
+}
+
+
+ex_comma_strings <- stringr::str_replace_all(mul_strings, ",", "*")
+
+formula_strings <- stringr::str_remove_all(ex_comma_strings, "[mul\\(, \\)]")
+
+numbers <- sapply(formula_strings, \(x) eval(parse(text=x))) 
+
+print(paste("result of enhanced multiplication", sum(numbers)))
